523db3c4ee28a3abbdb075e2d37c44a1493db768
Thomas WACHE <thomas.wache@intradef.gouv.fr>
2018-04-03 10:23:31 +0200

    Added GPL header in each file

--- src/colors.h
+++ src/colors.h
@@ -1,3 +1,21 @@
+/*
+ * rg, a small tool to find gadgets in a binary
+ * Copyright (C) 2018 mephesto1337
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __COLORS_H__
 #define __COLORS_H__
 
